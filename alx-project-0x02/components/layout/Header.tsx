14e57974920fee3a1d3db921428451ff4f0f454e
Donald-Edinam <kobbyedinamdonald@gmail.com>
2025-01-10 07:25:11 +0000

    Add: Included post route in header

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -18,5 +18,9 @@ const Header = () => {
     </div>
   )
 }
-
+<button
+  className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
+>
+  <Link href="/posts">Posts</Link>
+</button>
 export default Header
