ec9b30797b7df2935cada3d642c6a413f24ed7d9
Donald-Edinam <kobbyedinamdonald@gmail.com>
2025-01-11 06:07:21 +0000

    Fix error

--- alx-project-0x02/pages/users.tsx
+++ alx-project-0x02/pages/users.tsx
@@ -1,52 +1,42 @@
-import UserCard from '@/components/common/UserCard'
-import Header from '@/components/layout/Header'
-import { type UserProps } from '@/interfaces'
-import React from 'react'
+import UserCard from '@/components/common/UserCard';
+import Header from '@/components/layout/Header';
+import { type UserProps } from '@/interfaces';
 
-interface Props {
-    users: UserProps[]
-}
-
-const Users: React.FC<Props> = ({ users }) => {
-    return (
-        <div className="p-4">
-            <Header />
-            <h1 className="text-2xl font-bold mb-4">Ranked Users</h1>
-
-            <div className="space-y-4">
-                {users.map((user) => (
-                    <UserCard key={user.email}
-                        name={user.name}
-                        email={user.email}
-                        address={user.address}
-
-                    />
-                ))}
-            </div>
+const Users: React.FC<{
+  users: UserProps[];
+}> = ({ users }) => {
+  return (
+    <>
+      <Header />
+      <main className='px-5 md:px-10 lg:px-20'>
+        <div className='mb-7'>
+          <h1>Users page</h1>
         </div>
-    )
+        <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-8'>
+          {users?.map((user) => (
+            <UserCard
+              key={user.id}
+              name={user.name}
+              address={user.address}
+              email={user.email}
+            />
+          ))}
+        </div>
+      </main>
+    </>
+  );
+};
+
+export async function getStaticProps() {
+  const response = await fetch('https://jsonplaceholder.typicode.com/users');
+  const users = await response.json();
+  console.log(users, 'users');
+
+  return {
+    props: {
+      users,
+    },
+  };
 }
 
-export const getStaticProps = async () => {
-    try {
-        const response = await fetch('https://jsonplaceholder.typicode.com/users')
-        const users = await response.json()
-
-        return {
-            props: {
-                users,
-            },
-            // Revalidate every hour
-            revalidate: 3600,
-        }
-    } catch (error) {
-        console.error('Error fetching users:', error)
-        return {
-            props: {
-                users: [],
-            },
-        }
-    }
-}
-
-export default Users
+export default Users;
